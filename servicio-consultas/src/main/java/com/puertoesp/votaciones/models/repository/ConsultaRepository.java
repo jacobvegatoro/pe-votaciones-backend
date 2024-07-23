4daa90a6c1d3ec7036b754f38c855fb20fb0d78e
Jacob Vega <jacob.vega@gmail.com>
2024-07-23 09:31:04 -0400

    Eliminado servicio consultas

--- servicio-consultas/src/main/java/com/puertoesp/votaciones/models/repository/ConsultaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.puertoesp.votaciones.models.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.puertoesp.votaciones.models.entity.Consulta;
-
-public interface ConsultaRepository extends CrudRepository<Consulta, Long> {
-
-}
--- servicio-consultas/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO consultas (titulo, detalle, fecha_inicio, fecha_fin, estado) VALUES ('Consulta 1','Detalle consulta 1', NOW(), NOW(),1);
-INSERT INTO consultas (titulo, detalle, fecha_inicio, fecha_fin, estado) VALUES ('Consulta 2','Detalle consulta 2', NOW(), NOW(),2);
-INSERT INTO consultas (titulo, detalle, fecha_inicio, fecha_fin, estado) VALUES ('Consulta 3','Detalle consulta 3', NOW(), NOW(),1);
