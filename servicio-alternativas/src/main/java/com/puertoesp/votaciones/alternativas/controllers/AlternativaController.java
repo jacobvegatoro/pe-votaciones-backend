0923250cae09fdf958352f15aa62278beb419de1
Jacob Vega <jacob.vega@gmail.com>
2024-07-31 18:02:40 -0400

    Modificaciones gateway server

--- servicio-alternativas/src/main/java/com/puertoesp/votaciones/alternativas/controllers/AlternativaController.java
+++ servicio-alternativas/src/main/java/com/puertoesp/votaciones/alternativas/controllers/AlternativaController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.puertoesp.votaciones.alternativas.models.Alternativa;
@@ -17,7 +19,12 @@ public class AlternativaController {
     private AlternativaService alternativaService;
 
     @GetMapping("/listar")
-    public List<Alternativa> listar(){
+    public List<Alternativa> listar(
+        @RequestParam(name="nombre", required = false) String nombre,
+        @RequestHeader(name="token-request", required = false) String token
+    ){
+        System.out.println(nombre);
+        System.out.println(token);
         return alternativaService.getAll();
     }
 
