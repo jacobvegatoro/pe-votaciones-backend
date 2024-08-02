4095725b8320ccbbee8e830314f0852324bbd8b7
Jacob Vega <jacob.vega@gmail.com>
2024-08-02 17:56:23 -0400

    Implementación Circuit Breaker

--- servicio-boletas/src/main/java/com/puertoesp/votaciones/boletas/controllers/BoletaController.java
+++ servicio-boletas/src/main/java/com/puertoesp/votaciones/boletas/controllers/BoletaController.java
@@ -1,19 +1,33 @@
 package com.puertoesp.votaciones.boletas.controllers;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.puertoesp.votaciones.boletas.models.Boleta;
+import com.puertoesp.votaciones.boletas.models.Consulta;
 import com.puertoesp.votaciones.boletas.models.service.BoletaService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
 public class BoletaController {
+
+    private final Logger logger = LoggerFactory.getLogger(BoletaController.class);
+
+    @Autowired
+    private CircuitBreakerFactory cbFactory;
 
     @Autowired
     @Qualifier("serviceFeign") //esto se usa cuando se agrega un nombre al servicio 
@@ -26,7 +40,59 @@ public class BoletaController {
     
     @GetMapping("/ver/{id}/texto/{texto}")
     public Boleta detalle(@PathVariable Long id, @PathVariable String texto) {
+        //return boletaService.getById(id, texto);
+        return cbFactory.create("boletas")
+            .run(() -> boletaService.getById(id, texto), e -> metodoAlternativo(id, texto, e));
+    }
+    
+    @CircuitBreaker(name="boletas", fallbackMethod = "metodoAlternativo")
+    @GetMapping("/ver2/{id}/texto/{texto}")
+    public Boleta detalle2(@PathVariable Long id, @PathVariable String texto) {
         return boletaService.getById(id, texto);
+    }
+    
+    @TimeLimiter(name="boletas", fallbackMethod = "metodoAlternativo2")
+    @GetMapping("/ver3/{id}/texto/{texto}")
+    public CompletableFuture<Boleta> detalle3(@PathVariable Long id, @PathVariable String texto) {
+        return CompletableFuture.supplyAsync(() -> boletaService.getById(id, texto));
+    }
+
+    public Boleta metodoAlternativo(Long id, String texto, Throwable e){
+
+        logger.info(e.getMessage());
+
+        Boleta boleta = new Boleta();
+        Consulta consulta = new Consulta();
+
+        boleta.setTexto(texto);
+        consulta.setId(id);
+        consulta.setTitulo("Consulta X");
+        consulta.setDetalle("Detalle consulta X");
+        consulta.setEstado(1);
+        consulta.setFechaInicio(null);
+        consulta.setFechaFin(null);
+        boleta.setConsulta(consulta);
+
+        return boleta;
+    }
+
+    public CompletableFuture<Boleta> metodoAlternativo2(Long id, String texto, Throwable e){
+
+        logger.info(e.getMessage());
+
+        Boleta boleta = new Boleta();
+        Consulta consulta = new Consulta();
+
+        boleta.setTexto(texto);
+        consulta.setId(id);
+        consulta.setTitulo("Consulta X");
+        consulta.setDetalle("Detalle consulta X");
+        consulta.setEstado(1);
+        consulta.setFechaInicio(null);
+        consulta.setFechaFin(null);
+        boleta.setConsulta(consulta);
+
+        return CompletableFuture.supplyAsync(() -> boleta);
     }
 
 }
--- servicio-consultas/src/main/java/com/puertoesp/votaciones/consultas/controllers/ConsultaController.java
+++ servicio-consultas/src/main/java/com/puertoesp/votaciones/consultas/controllers/ConsultaController.java
@@ -1,11 +1,12 @@
 package com.puertoesp.votaciones.consultas.controllers;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.core.env.Environment;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,8 @@ import com.puertoesp.votaciones.consultas.models.service.IConsultaService;
 @RestController
 public class ConsultaController {
 
-    //@Autowired
-    //private Environment env;
+    @Autowired
+    private Environment env;
 
     @Value("${server.port}")
     private Integer port;
@@ -29,17 +30,26 @@ public class ConsultaController {
     public List<Consulta> listar(){
         //return consultaService.getAll();
         return consultaService.getAll().stream().map(consulta -> {
-            //consulta.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-            consulta.setPort(port);
+            consulta.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+            //consulta.setPort(port);
             return consulta;
         }).collect(Collectors.toList());
     }
 
     @GetMapping("/ver/{id}")
-    public Consulta detalle(@PathVariable Long id){
+    public Consulta detalle(@PathVariable Long id) throws InterruptedException{
+
+        if (id.equals(10L)){
+            throw new IllegalStateException("Producto no encontrado!");
+        }
+
+        if (id.equals(3L)){
+            TimeUnit.SECONDS.sleep(5L);
+        }
+
         Consulta consulta = consultaService.getById(id);
-        //consulta.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-        consulta.setPort(port);
+        consulta.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+        //consulta.setPort(port);
         return consulta;
     }
 
