732b8c3c885a1a72096b7b50397cf4f3f16a6bfa
Jacob Vega <jacob.vega@3it.cl>
2025-08-18 16:15:02 -0400

    Se actualiza la base de datos

--- servicio-consultas/src/main/resources/import.sql
+++ servicio-consultas/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO consultas (titulo, detalle, fecha_inicio, fecha_fin, estado) VALUES ('Consulta 1','Detalle consulta 1', NOW(), NOW(),1);
-INSERT INTO consultas (titulo, detalle, fecha_inicio, fecha_fin, estado) VALUES ('Consulta 2','Detalle consulta 2', NOW(), NOW(),2);
-INSERT INTO consultas (titulo, detalle, fecha_inicio, fecha_fin, estado) VALUES ('Consulta 3','Detalle consulta 3', NOW(), NOW(),1);
+INSERT INTO consultas (titulo, detalle, fecha_inicio, fecha_fin, estado) VALUES ('Consulta 1','Detalle consulta 1', NOW(), DATE_ADD(NOW(), INTERVAL 10 DAY),1);
+INSERT INTO consultas (titulo, detalle, fecha_inicio, fecha_fin, estado) VALUES ('Consulta 2','Detalle consulta 2', NOW(), DATE_ADD(NOW(), INTERVAL 5 DAY),2);
+INSERT INTO consultas (titulo, detalle, fecha_inicio, fecha_fin, estado) VALUES ('Consulta 3','Detalle consulta 3', NOW(), DATE_ADD(NOW(), INTERVAL 3 DAY),1);
